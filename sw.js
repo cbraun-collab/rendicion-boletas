ef80b87037201cd35ff8f9842779c6309ff6a18c
cbraun-collab <cbraun@senercom.cl>
2026-06-25 09:56:02 -0300

    Agregar app rendición boletas

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'rendicion-boletas-v1';
+const STATIC = [
+  '/rendicion-boletas/',
+  '/rendicion-boletas/index.html',
+  '/rendicion-boletas/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Network-first
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(resp => {
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return resp;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
